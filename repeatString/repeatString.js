b28cfa2999faf72634f92adf22918027cf9c786f
Ji Chung <jwchungp@gmail.com>
2020-12-24 09:59:52 -0800

    utilizing for loops to fix repeatString test cases

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, counter) {
+  if (counter < 0) {
+    return 'ERROR';
+  }
+  answer = '';
+  for (let i = 0; i < counter; i++) {
+    answer += str;
+  }
+  return answer;
 }
 
 module.exports = repeatString
