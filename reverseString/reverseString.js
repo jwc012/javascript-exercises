d6f12ed04c94008a51fac1980d604d8f12e0389e
Ji Chung <jwchungp@gmail.com>
2020-12-25 19:53:13 -0800

    finish fixing reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  if (str.length < 2) return str;
+  let answer = "";
+  for (let lastIndex = str.length - 1; lastIndex >= 0; lastIndex--) {
+    answer += str.charAt(lastIndex);
+  }
+  return answer;
 }
 
 module.exports = reverseString
