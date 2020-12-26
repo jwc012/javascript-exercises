a267917287f21d6ff3e3fc0172d0d28144873682
Ji Chung <jwchungp@gmail.com>
2020-12-25 20:20:19 -0800

    fixed removing an element from an array utilizing two for loops O(n^2) speed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numbers) {
+  if (array == null || array.length == 0) return array;
+  for (let i = 0; i < numbers.length; i++) {
+    let removeNum = numbers[i];
+    for (let j = 0; j < array.length; j++) {
+      let currentNum = array[j];
+      if (currentNum === removeNum) {
+        array.splice(j, 1);
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = removeFromArray
