a49add93ee01dc8135d5c6380d36053c99ac2b11
Ji Chung <jwchungp@gmail.com>
2020-12-25 20:33:31 -0800

    passing all sumAll test cases

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 !== parseInt(num1, 10) || num2 !== parseInt(num2, 10) || num1 < 0 || num2 < 0) {
+    return "ERROR"
+  }
+  let lowerNum = Math.min(num1, num2);
+  let higherNum = Math.max(num1, num2);
+  let sum = 0;
+  for (; lowerNum <= higherNum; lowerNum++){
+    sum += lowerNum;
+  }
+  return sum;
 }
 
 module.exports = sumAll
