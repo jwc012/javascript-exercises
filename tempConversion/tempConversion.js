c73ce4e77d846c1c344fc495ead66a226f6498ea
Ji Chung <jwchungp@gmail.com>
2020-12-27 19:23:25 -0800

    passes tempConversion test cases

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
 const ftoc = function(fahrenheit) {
-
+  let celsius = (fahrenheit - 32) * (5/9);
+  if (celsius % 1 === 0) {
+    return celsius;
+  }
+  return +celsius.toFixed(1);
 }
 
 const ctof = function(celsius) {
-
+  let fahrenheit = celsius * 1.8 + 32;
+  if (fahrenheit % 1 === 0) {
+    return fahrenheit;
+  }
+  // utilizing + unary to convert string into number
+  return +fahrenheit.toFixed(1);
 }
 
 module.exports = {
