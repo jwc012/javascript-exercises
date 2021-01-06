195f0fed71738ca4e2afc7bae0de43e2bae7d857
Ji Chung <jwchungp@gmail.com>
2021-01-05 19:25:10 -0800

    passes all js palindromes test cases

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+  if (word == null || word.length == 0) return true;
+  let start = 0; 
+  let end = word.length - 1; 
+  while (start < end) {
+    while (start < end && !(/[a-zA-Z]/).test(word.charAt(start))) start++;
+    while (start < end && !(/[a-zA-Z]/).test(word.charAt(end))) end--;
+    if (word.charAt(start).toLowerCase() != word.charAt(end).toLowerCase()) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 }
 
 module.exports = palindromes
