bf0dea634b0e58699f3f5906ea8c08b47f429fdd
Ji Chung <jwchungp@gmail.com>
2021-01-12 14:56:18 -0800

    passes all caesar test cases

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
+const caesar = function(word, num) {
+  let decipher = '';
 
+  if (num < 0) {
+    return caesar(word, num + 26);
+  }
+
+  for (let i = 0; i < word.length; i++) {
+    let asciiValue = word.charCodeAt(i);
+
+    // Uppercase letters
+    if ((/[A-Z]/).test(word.charAt(i))) {
+      asciiValue = (asciiValue - 65 + num) % 26 + 65
+    }
+    // Lowercase letters 
+    else if ((/[a-z]/).test(word.charAt(i))) {
+      asciiValue = (asciiValue - 97 + num) % 26 + 97
+    }
+    // converts ascii code to characters
+    decipher += String.fromCharCode(asciiValue);
+  }
+
+  return decipher;
 }
 
 module.exports = caesar
