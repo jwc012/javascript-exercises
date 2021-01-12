aad607e21054f76893ef4bb92a3fd44235a92619
Ji Chung <jwchungp@gmail.com>
2021-01-12 15:34:05 -0800

    passes all calculator test cases

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (numOne, numTwo) {
+	return numOne + numTwo;
 }
 
-function subtract () {
-	
+function subtract (numOne, numTwo) {
+	return numOne - numTwo;
 }
 
-function sum () {
-	
+function sum (nums) {
+	let sum = 0;
+	nums.forEach(num => {
+		sum += num;
+	});
+
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (nums) {
+	let total = 1;
+	nums.forEach(num => {
+		total *= num;
+	});
+
+	return total;
 }
 
-function power() {
-	
+function power(numOne, numTwo) {
+	return Math.pow(numOne, numTwo);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num == 0) return 1;
+
+	return num * factorial(num - 1);
 }
 
 module.exports = {
